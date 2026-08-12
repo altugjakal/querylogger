945a5818c21d5301492eff791385f6061538b34f
Mehmet Altuğ JAKAL <98031676+crumbsws@users.noreply.github.com>
2026-08-12 18:11:05 +0300

    Return data

--- js/app.js
+++ js/app.js
@@ -340,15 +340,19 @@ async function search() {
     'query': complete_query,
     'keystrokes': rawKeystrokeBuffer
   };
-  const data = await storeApi(json_string).then(r => console.log(r));
+  const data = await storeApi(json_string).then(r => {
+    console.log(r);
+    return r;
+  });
   searchInput.value = '';
   rawKeystrokeBuffer = [];
-  switchTask()
+
   resultsField.innerHTML = data.map(item => `
     <div class="result-card">
         <a href="${item.url}" target="_blank">${item.title}</a>
     </div>
 `).join('');
+  switchTask()
 
 }
 
